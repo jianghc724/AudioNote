dc6588cafeae086e4a3bd113421cddd7cfeeb0d9
DESKTOP-H26ORQ8\JiangHaochen <jianghc724@foxmail.com>
2017-11-05 02:10:39 +0800

    Revert "trivial model"
    This reverts commit 6e67ddbe3b1a9b1f534a6123f11ef5c33f98ac9e.

--- speech_to_text.py
+++ speech_to_text.py
@@ -6,44 +6,7 @@ import os
 
 class Converter:
     def convert(self, file_name):
-        r = sr.Recognizer()
-        with sr.AudioFile(file_name) as source:
-            audio = r.record(source)
-        print("start recognize")
-
-#        try:
-#            print("Sphinx thinks you said "+ r.recognize_sphinx(audio, language='zh-CN'))
-#        except sr.UnknownValueError:
-#            print("Sphinx could not understand audio")
-#        except sr.RequestError as e:
-#            print("Sphinx error; {0}".format(e))
-
-#        try:
-#            print("Google Speech Recognition thinks you said " + r.recognize_google(audio, language='zh-CN'))
-#        except sr.UnknownValueError:
-#            print("Google Speech Recognition could not understand audio")
-#        except sr.RequestError as e:
-#            print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-#        IBM_USERNAME = "38de5d99-7cf7-445a-ab1a-b138b98cbb86"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-#        IBM_PASSWORD = "VFdRan6vNf3P"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
-#        try:
-#            print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME,
-#                                                                          password=IBM_PASSWORD, language='zh-CN'))
-#        except sr.UnknownValueError:
-#            print("IBM Speech to Text could not understand audio")
-#        except sr.RequestError as e:
-#            print("Could not request results from IBM Speech to Text service; {0}".format(e))
-
-        GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"abc"
-        try:
-            print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio,
-                                                                                    credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
-        except sr.UnknownValueError:
-            print("Google Cloud Speech could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results from Google Cloud Speech service; {0}".format(e))
-
+        pass
 
 if __name__ == '__main__':
     data = Converter()
